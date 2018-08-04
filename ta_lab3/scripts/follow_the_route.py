53f11785b214894213bac9789dc21c8a4bb29ccf
Demidov Notebook <demidov.d@starline.ru>
2018-08-04 19:28:02 +0300

    wall distance to followe from odometry

--- ta_lab3/scripts/follow_the_route.py
+++ ta_lab3/scripts/follow_the_route.py
@@ -4,6 +4,7 @@ import rospy
 import yaml
 
 import sys
+import math
 
 from take_photo import TakePhoto
 from go_to_specific_point_on_map import GoToPose
@@ -17,10 +18,36 @@ from wall_follow import WallFollow
 RIGHT = 'right'
 LEFT  = 'left'
 
+old_x = 0.0
+old_y = 0.0
+
+distance = 0.0
+
+wall_session = False
+
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry
 
 def amcl_pose_callback(msg):
     print msg.pose.pose
+    
+def odometry_callback(msg):
+    global old_x
+    global old_y
+    global distance
+    
+    new_x = msg.pose.pose.position.x
+    new_y = msg.pose.pose.position.y
+
+    if wall_session == False:
+        old_x = new_x
+        old_y = new_y 
+        distance = 0.0
+        
+    if wall_session == True:
+        p1 = (new_x, new_y)
+        p2 = (old_x, old_y)
+        distance = math.hypot(p2[0] - p1[0], p2[1] - p1[1])
 
 if __name__ == '__main__':
 
@@ -29,7 +56,10 @@ if __name__ == '__main__':
     try:
         rospy.init_node('follow_route', anonymous=False)
         
+        global wall_session
+        
         rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, amcl_pose_callback)
+        rospy.Subscriber("odom", Odometry, odometry_callback)
         
         start_session_pub = rospy.Publisher('photo/start_session', Bool, queue_size=10)
         session_state_msg = Bool()
@@ -63,16 +93,22 @@ if __name__ == '__main__':
             rack_id_pub.publish(rack_id_msg)
 
             if obj['rackid'] != 0:
-		rospy.loginfo("Start wall")
+                rospy.loginfo("Start wall")
+                wall_session = True
                 wall.start()
             
-                rospy.sleep(20)
-		rospy.loginfo("Stop wall")                
+                while distance < 3.0:
+                    rospy.loginfo("Distance is: %s", distance)
+                    rospy.sleep(0.1)
+                    
+                rospy.loginfo("Stop wall")                
                 wall.stop()
+                wall_session = False
+                
             else:
-	        rospy.loginfo("Start navi")
+                rospy.loginfo("Start navi")
                 success = navigator.goto(obj['position'], obj['quaternion'])
-            
+
                 rospy.loginfo("Stop navi")
                 if not success:
                     rospy.loginfo("Failed to reach %s pose", name[:-4])
